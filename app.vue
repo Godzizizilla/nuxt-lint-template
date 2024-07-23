84d728be3744efda495380b4e0ab9fe34663e6d0
Godzilla <2439396101@qq.com>
2024-07-23 18:05:09 +0800

    chore: add scripts

--- app.vue
+++ app.vue
@@ -4,3 +4,16 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup></script>
+
+<style lang="scss">
+  .el-tabs__nav {
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #dbe3ea !important;
+    border-radius: 8px !important;
+    box-sizing: border-box !important;
+    padding: 1px;
+  }
+</style>
--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,13 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
-import { includeIgnoreFile } from "@eslint/compat"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const gitignorePath = path.resolve(__dirname, ".gitignore")
+const gitignorePath = path.resolve(__dirname, '.gitignore')
 
 export default [
   includeIgnoreFile(gitignorePath),
